30577e2d3d888ff6aa9ebc243269ae8795cbf627
DEVNET\d1357847 <fengming.teong@ncs.com.sg>
2024-03-01 20:35:44 +0800

    added Jhipster react frontend

---
+++ Frontend/src/main/webapp/app/modules/login/login-reducer.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:8080/', // Base URL of your Java backend
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export const getSomeData = async () => {
+    try {
+        const response = await apiClient.get('user/test');
+        return response.data;
+    } catch (error) {
+        throw new Error('Error fetching data');
+    }
+};
--- springbootWithPostgresql/src/main/java/com/example/springbootWithPostgresql/controller/AccountController.java
+++ springbootWithPostgresql/src/main/java/com/example/springbootWithPostgresql/controller/AccountController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:9001")
 @RequestMapping("/account")
 public class AccountController {
 
