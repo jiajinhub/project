213b97152165685a1afd3f7da0ef7d39f8e39798
DEVNET\d1357847 <fengming.teong@ncs.com.sg>
2024-03-03 15:31:07 +0800

    setup backend base url

---
+++ Frontend/src/main/webapp/app/config/constants/api-endpoints.ts
@@ -0,0 +1,6 @@
+//Backend server API_URL
+export const API_URL = BACKEND_SERVER_API_URL;
+
+//Account
+export const API_GET_ACCOUNT = 'account/Retrieve';
+export const API_POST_ACCOUNT_AUTHENTICATE = 'account/authenticate';
--- Frontend/src/main/webapp/app/typings.d.ts
+++ Frontend/src/main/webapp/app/typings.d.ts
@@ -1,6 +1,7 @@
 declare const VERSION: string;
 declare const SERVER_API_URL: string;
 declare const DEVELOPMENT: string;
+declare const BACKEND_SERVER_API_URL: string;
 
 declare module '*.json' {
   const value: any;
