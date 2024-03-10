3b9a1004dfd26bbfba71e2979dfddfc7485e3728
DEVNET\d1357847 <fengming.teong@ncs.com.sg>
2024-03-10 16:52:29 +0800

    fix logout

--- Frontend/src/main/webapp/app/modules/login/logout.tsx
+++ Frontend/src/main/webapp/app/modules/login/logout.tsx
@@ -1,17 +1,19 @@
 import React, { useLayoutEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
+import { reset } from './login.reducer';
 //import { logout } from 'app/shared/reducers/authentication';
 
 export const Logout = () => {
-  const logoutUrl = useAppSelector(state => state.authentication.logoutUrl);
+  //const logoutUrl = useAppSelector(state => state.authentication.logoutUrl);
   const dispatch = useAppDispatch();
 
   useLayoutEffect(() => {
     //dispatch(logout());
-    if (logoutUrl) {
-      window.location.href = logoutUrl;
-    }
+    //if (logoutUrl) {
+    // window.location.href = logoutUrl;
+    //}
+    dispatch(reset());
   });
 
   return (
