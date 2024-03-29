ad6a43ab888a28744430d4e69e788b0f53fcadd2
andy <andy.koh@ncs.com.sg>
2024-03-29 21:36:46 +0800

    change to Integer

--- springbootWithPostgresql/src/main/java/com/example/springbootWithPostgresql/repository/ProdRepo.java
+++ springbootWithPostgresql/src/main/java/com/example/springbootWithPostgresql/repository/ProdRepo.java
@@ -4,5 +4,5 @@ package com.example.springbootWithPostgresql.repository;
 import com.example.springbootWithPostgresql.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdRepo extends JpaRepository<ProductEntity, Long> {
+public interface ProdRepo extends JpaRepository<ProductEntity, Integer> {
 }
